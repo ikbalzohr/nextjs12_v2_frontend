2ae64b919b76ac30b9da25277bad09169bb01abb
ikbalzohr <ikbal.gf12@gmail.com>
2022-05-31 20:07:25 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    // MONGO_URI: "https://backend-nodejs-lilac.vercel.app/users",
     MONGO_URI: "http://localhost:4000/v1/users/list",
     MONGO_IMG: "http://localhost:4000/",
+
+    // MONGO_URI: "https://api-nodejs-server.vercel.app/v1/users/list",
+    // MONGO_IMG: "https://api-nodejs-server.vercel.app/",
   },
 };
 
