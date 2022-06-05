db0a414b795afcbde570acf226ee29d8402604e7
ikbalzohr <ikbal.gf12@gmail.com>
2022-06-05 10:10:11 +0800

    update front

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Sidebar from "../components/sideBar";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <>
       <Sidebar />
+      <div className="container text-center cursor-pointer">
+        <Link href={"/userlist"}>
+          <h1 className="text-3xl p-24 font-bold text-fuchsia-500"> Go to Users List</h1>
+        </Link>
+      </div>
     </>
   );
 };
